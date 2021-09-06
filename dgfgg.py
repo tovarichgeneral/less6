8fe05993a1937e27336a3697e690fcf11aca9345
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2021-09-06 18:00:03 +0500

    Fix code style

--- dgfgg.py
+++ dgfgg.py
@@ -1,12 +1,17 @@
-try:
-    my_file = open("text/push.text.txt", "rb")
-    my_file_write = open("text/write.txt", "wb")
-    while True:
-        cont = my_file.read(1024)
-        if not cont:
-            break
-        my_file_write.write(cont)
-    my_file.close()
-    my_file_write.close()
-except FileNotFoundError:
-    print("Error")
+def main():
+    try:
+        my_file = open("text/push.text.txt", "rb")
+        my_file_write = open("text/write.txt", "wb")
+        while True:
+            cont = my_file.read(1024)
+            if not cont:
+                break
+            my_file_write.write(cont)
+        my_file.close()
+        my_file_write.close()
+    except FileNotFoundError:
+        print("Error")
+
+
+if __name__ == '__main__':
+    main()
